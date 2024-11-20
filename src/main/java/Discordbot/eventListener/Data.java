07503edcd222631c4b09dd2248f0e702c0792fae
Alexander Eide <atlexeide@gmail.com>
2024-11-20 19:14:46 +0100

    Refactor Data class

--- src/main/java/Discordbot/eventListener/Data.java
+++ src/main/java/Discordbot/eventListener/Data.java
@@ -4,20 +4,8 @@ import java.util.Objects;
 
 public class Data {
     private static int onlinePlayers;
+    public int playerCount;
     public String player;
     public String event;
-    public int playerCount;
-    public Data(String _player, String _event) {
-        player = _player;
-        event = _event;
-        if(Objects.equals(_event, "PlayerJoinEvent")){
-           onlinePlayers++;
-        }if(Objects.equals(_event, "PlayerLeaveEvent")){
-            onlinePlayers--;
-        }
-        playerCount = getPlayerCount();
-    }
-    public static int getPlayerCount (){
-        return onlinePlayers;
-    }
+    public String message;
 }
