addfe15f214a186780baca1ab25d5190360ce545
Alexander Eide <atlexeide@gmail.com>
2024-11-21 11:46:49 +0100

    Add fields to Data class

--- src/main/java/Discordbot/eventListener/Data.java
+++ src/main/java/Discordbot/eventListener/Data.java
@@ -1,11 +1,15 @@
 package Discordbot.eventListener;
 
 public class Data {
+    public String ip;
+    public String port;
     public static int onlinePlayers;
     public int playerCount = onlinePlayers;
+
     public String player;
+    public String messsage;
     public String event;
-    public String message;
+    public String motd;
     public String gamemode;
     public String newGamemode;
 }
