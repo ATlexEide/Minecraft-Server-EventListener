4603d361b40e597ef3ddd346bc52dcd61c7fe583
Alexander Eide <atlexeide@gmail.com>
2024-11-23 01:09:56 +0100

    i dont even know

--- src/main/java/Discordbot/eventListener/Server.java
+++ src/main/java/Discordbot/eventListener/Server.java
@@ -1,6 +1,4 @@
 package Discordbot.eventListener;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.bukkit.Bukkit;
 import java.io.IOException;
 import java.net.URI;
@@ -9,20 +7,32 @@ import java.net.http.*;
 
 
 public class Server {
+    static HttpClient client = HttpClient.newHttpClient();
+
     static void send(String data){
         try {
             Bukkit.getServer().getLogger().info(data);
 
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
+            HttpRequest postRequest = HttpRequest.newBuilder()
                     .uri(URI.create("http://localhost:3000/events"))
                     .header("Content-Type", "application/json")
                     .POST(HttpRequest.BodyPublishers.ofString(data))
                     .build();
-
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
+            HttpResponse<String> postResponse = client.send(postRequest, HttpResponse.BodyHandlers.ofString());
+            HttpRequest getRequest = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:3000/server/chat"))
+                    .build();
+            HttpResponse getResponse = client.sendAsync(getRequest, HttpResponse.BodyHandlers.ofString()).join();
+            Bukkit.getLogger().info(getResponse.toString());
+            if(getResponse.statusCode() == 200){
+                Bukkit.getServer().broadcastMessage(getResponse.body().toString());
+            }
     }catch(IOException | InterruptedException e){
         Bukkit.getLogger().severe("Error sending data");
     }
-}}
+}
+
+
+
+
+}
